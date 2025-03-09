b6becd4938988645b1e767b377f7e8b1ec2bbda9
piyushjha0409 <peeyush0409@gmail.com>
2025-03-09 18:29:55 +0530

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,31 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export const middleware = async (request: NextRequest) => {
-  const token = await getToken({ req: request });
-  const url = request.nextUrl;
+export default withAuth(
+  function middleware(req) {
+    const { pathname } = req.nextUrl;
+    const isAuthenticated = !!req.nextauth.token; // Checks if a user is authenticated
 
-  // Redirect authenticated users away from sign-in, register, and home pages
-  if (token) {
-    if (
-      url.pathname.startsWith("/login") ||
-      url.pathname.startsWith("/register") ||
-      url.pathname === "/"
-    ) {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
+    const protectedRoutes = ["/upload", "/dashboard"];
+    const publicRoutes = ["/", "/login", "/register"];
+
+    if (!isAuthenticated && protectedRoutes.includes(pathname)) {
+      return NextResponse.redirect(new URL("/login", req.url));
     }
-  } else {
-    // Redirect unauthenticated users to the sign-in page if they try to access protected routes
-    if (url.pathname.startsWith("/dashboard")) {
-      return NextResponse.redirect(new URL("/signin", request.url));
+
+    if (isAuthenticated && publicRoutes.includes(pathname)) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
     }
+
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token, // Only allow authenticated users
+    },
   }
-
-  // Allow the request to proceed if no redirection is needed
-  return NextResponse.next();
-};
+);
 
 export const config = {
-  matcher: [
-    "/login",
-    "/register",
-    "/dashboard",
-    "/dashboard/:path*", // Apply middleware to all subroutes of /dashboard
-  ],
+  matcher: ["/upload", "/dashboard", "/", "/login", "/register"],
 };
